3a43aa3049c11398b3e052795a868b50630c4269
Ronik Karki <ronik.carkey@gmail.com>
2019-02-01 13:31:48 +0545

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,14 +1,9 @@
 from selenium import webdriver
-import time
 from time import sleep
 import os
 import csv
 import argparse
-
-
-ts=time.time()
-
-
+import time
 from selenium.common.exceptions import NoSuchElementException
 
 path= r'./Record'
@@ -16,8 +11,7 @@ if not os.path.exists(path):
     os.makedirs(path)
 #Importing files
 
-###Enabling headless browser
-
+###Enabling headless browser and passing argument
 option=webdriver.ChromeOptions()
 parser = argparse.ArgumentParser()
 parser.add_argument('--headless', help='for headless argument',action="store_true")
@@ -34,95 +28,134 @@ browser.get('https://www.google.com/search?hl=en&tbm=lcl&ei=qkFRXOXnNdf69QOJ07Co
 
 
 
-csv_file=open('Record/'+str(ts)+'Scrape.csv','w')
-csv_writer=csv.writer(csv_file)
-csv_writer.writerow(['Restaurant Name','Restaurant Rating','Restaurant Address','Contact No.','Cuisine','Opening Hours','Google Reviews'])
+ts=time.time()
+scrape_file=open('Record/'+str(ts)+'Scrape.csv','w')
+scrape_csv_writer=csv.writer(scrape_file)
+scrape_csv_writer.writerow(['Restaurant Name','Restaurant Rating','Restaurant Address','Contact No.','Cuisine','Opening Hours','Google Reviews'])
+
+review_file=open('Record/'+str(ts)+'Review.csv','w')
+review_csv_writer=csv.writer(review_file)
 
 
-csv_file1=open('Record/'+str(ts)+'Review.csv','w')
-csv_writer1=csv.writer(csv_file1)
+def browser_initialization():
+    print('THE NAME OF SCRAPED RESTAURANTS WITH ITS DATA ARE AS FOLLOWS:')
 
-print('THE NAME OF SCRAPED RESTAURANTS WITH ITS DATA ARE AS FOLLOWS:')
-for j in range(13):
-    for i in range(19):
-        try:
-            browser.find_elements_by_class_name("VkpGBb")[i].click();
-        except:
-            continue
 
-        sleep(5)
-        try:
-            browser.find_element_by_class_name("BTP3Ac").click();
-        except:
-            continue
+def clicking_buttons_to_load_required_data():
+    try:
+        browser.find_elements_by_class_name("VkpGBb")[i].click();
+    except:
+        pass
 
-        #Scraping Name
-        try:
-            name=browser.find_element_by_class_name("SPZz6b").get_attribute('innerText')
-            print(name)
-        except NoSuchElementException:
-            name='Not Provided'
-    #Scraping rating
-        try:
-            rating=browser.find_elements_by_class_name("BTtC6e")[i].get_attribute('innerText')
-    #print(rating)
-        except NoSuchElementException:
-            rating='Not Provided'
-        #Scraping Address
-        try:
-            address=browser.find_element_by_class_name("LrzXr").get_attribute('innerText')
-    #print(address)
-        except NoSuchElementException:
-            address="Not Provided"
-
-    #Scraping Contact
-        try:
-            contact=browser.find_element_by_css_selector('.zdqRlf').get_attribute('innerText')
-        #print(contact)
-        except NoSuchElementException:
-            contact="Not Provided"
-
-        #Scraping cuisine
-        try:
-            cuisine=browser.find_element_by_css_selector('span.YhemCb:nth-child(2)').get_attribute('innerText')
-            #print(cuisine)
-        except NoSuchElementException:
-            cuisine='Not Provided'
-
-        #scraping Opening Hours
-        try:
-            sleep(2)
-            time=browser.find_element_by_class_name('WgFkxc').get_attribute('innerText')
-           # print(time)
-        except NoSuchElementException:
-            time='Not Provided'
-
-        #scraping google review
-        try:
-            google_review=browser.find_element_by_css_selector("span.fl:nth-child(3)").get_attribute('innerText')
-           # print(google_review)
-        except NoSuchElementException:
-            google_review='Not Provided'
-
-        #scraping comments review
-        csv_writer1.writerow(['Restaurant Name:'+name])
-        for reviews in range(3):
-            try:
-                review=browser.find_elements_by_class_name('jxjCjc')[reviews].get_attribute('innerText')
-
-            except:
-                continue
-
-            csv_writer1.writerow([review])
-
-        csv_writer.writerow([name,rating,address,contact,cuisine,time,google_review])
-
-    if j==13:
-        #print('loop break as page ends here')
-        break
-    browser.find_element_by_id("pnnext").click();
     sleep(5)
+    try:
+        browser.find_element_by_class_name("BTP3Ac").click();
+    except:
+        pass
 
-csv_file.close()
+def scrape_restaurants_name():
+    try:
+        name=browser.find_element_by_class_name("SPZz6b").get_attribute('innerText')
+        return(name)
+        print(name)
+    except NoSuchElementException:
+        name='Not Provided'
+        return(name)
+
+def scrape_restaurants_rating():
+    try:
+        rating=browser.find_elements_by_class_name("BTtC6e")[i].get_attribute('innerText')
+        return(rating)
+    #print(rating)
+    except NoSuchElementException:
+        rating='Not Provided'
+        return(rating)
+
+
+def scrape_restaurants_address():
+    try:
+        address=browser.find_element_by_class_name("LrzXr").get_attribute('innerText')
+        return(address)
+    #print(address)
+    except NoSuchElementException:
+        address="Not Provided"
+        return(address)
+
+def scrape_restaurants_contact():
+    try:
+        contact=browser.find_element_by_css_selector('.zdqRlf').get_attribute('innerText')
+        return(contact)
+        #print(contact)
+    except NoSuchElementException:
+        contact="Not Provided"
+        return(contact)
+
+def scrape_restaurants_cuisine():
+    try:
+        cuisine=browser.find_element_by_css_selector('span.YhemCb:nth-child(2)').get_attribute('innerText')
+        return(cuisine)
+            #print(cuisine)
+    except NoSuchElementException:
+        cuisine='Not Provided'
+        return(cuisine)
+
+def scrape_restaurants_opening_hours():
+    try:
+        sleep(2)
+        time=browser.find_element_by_class_name('WgFkxc').get_attribute('innerText')
+        return(time)
+       # print(time)
+    except NoSuchElementException:
+        time='Not Provided'
+        return(time)
+
+def scrape_restaurants_google_review():
+    try:
+        google_review=browser.find_element_by_css_selector("span.fl:nth-child(3)").get_attribute('innerText')
+        return(google_review)
+           # print(google_review)
+    except NoSuchElementException:
+        google_review='Not Provided'
+        return(google_review)
+
+def scrape_comments_review(name):
+    review_csv_writer.writerow(['Restaurant Name:'+name])
+    for reviews in range(3):
+        try:
+            review=browser.find_elements_by_class_name('jxjCjc')[reviews].get_attribute('innerText')
+
+        except:
+            continue
+        review_csv_writer.writerow([review])
+
+
+
+
+#################################################      MAIN    #####################################
+
+if __name__ == "__main__":
+    for j in range(13):
+        for i in range(19):
+            clicking_buttons_to_load_required_data()
+            name=scrape_restaurants_name()
+            print(name)
+            rating=scrape_restaurants_rating()
+            address=scrape_restaurants_address()
+            contact=scrape_restaurants_contact()
+            cuisine=scrape_restaurants_cuisine()
+            time=scrape_restaurants_opening_hours()
+            google_review=scrape_restaurants_google_review()
+            scrape_csv_writer.writerow([name,rating,address,contact,cuisine,time,google_review])
+            scrape_comments_review(name)
+        if j==13:
+            break
+        #print('loop break as page ends here')
+
+        browser.find_element_by_id("pnnext").click();
+        sleep(5)
+
+
+
+scrape_file.close()
+review_file.close()
 browser.quit()
-
